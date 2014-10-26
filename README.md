e2bc9fd92ae9b840dde2f7d8047ec367eb113696
jvalen <javiervalenciaromero@gmail.com>
2014-10-27 00:27:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Canvas Terminal
+=============
+
+The aim of this project is to play around with canvas and javascript through a custom command-line interface/terminal.
+
+How it works will be detailed soon, please be patient :)
+
+Thank you!
+
---
+++ test.js
@@ -0,0 +1,26 @@
+function init() {
+    var mainTerminalId = canvasTerminal.create(
+        { 
+            'canvasId': {
+                'cText': 'terminal-text-canvas', 
+                'cBg': 'terminal-bg-canvas'
+            },
+            'xOffset': 10,
+            'yOffset': 10,
+            'textSpeed': 10,
+            'backgroundAnimation': {
+                type: 'l-r',
+                speed: 10
+            },
+            'opacity': 0.8,
+            'title': "~ Canvas Terminal Demo ~",
+            'fontSize': 15,
+            'fontColor': '1EA900',
+            'backgroundColor': '000000'
+        }
+    );    
+    
+    canvasTerminal.startTerminal(mainTerminalId);
+}
+
+
